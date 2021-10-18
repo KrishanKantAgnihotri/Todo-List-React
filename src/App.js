6def70e962b13e13117a0a288e3c29e1904d224b
krishnaagnihotri11@gmail.com <krishnaagnihotri11@gmail.com>
2021-10-18 16:04:51 +0530

    Structure Created

--- src/App.js
+++ src/App.js
@@ -3,13 +3,36 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  const [name,setName]= useState("");
-  const [list,setList] = useState([]);
-  const [isEditing,setIsEditing] = useState(null);
-  const [alert,setAlert] = useState({show:false,msg:'',type:""});
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: "" });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
   return (
     <section className="section-center">
-      <div className="main-container"></div>
+      <form className="main-form" onSubmit={handleSubmit}>
+        {
+          alert.show && <Alert />
+        }
+        <h3>Todo List</h3>
+        <div className="form-control">
+          <input type="text" className="main" placeholder="e.g HomeWork" value={name} onChange={(e) => setName(e.target.value)} />
+          <button type="submit" className="submit-btn">
+            {
+              isEditing ? "edit" : 'Submit'
+            }
+          </button>
+        </div>
+      </form>
+      <div className="main-container">
+        <List />
+        <button className="clear-btn">
+          Clear Items
+        </button>
+      </div>
     </section>
   );
 }
